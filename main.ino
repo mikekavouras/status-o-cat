40c6a4ffec0e545860536f78f472b8bfd45acf29
Mike Kavouras <kavourasm@gmail.com>
2019-02-06 14:55:54 -0500

    strcmp

--- main.ino
+++ main.ino
@@ -7,10 +7,10 @@ SYSTEM_MODE(AUTOMATIC);
 #define PIXEL_COUNT 1
 #define PIXEL_TYPE WS2812
 
-enum Status { good, minor, major };
-Status currentStatus = good;
+enum Status { good, minor, major};
+Status status = Status::good;
 
-Thread thread("testThread", fetchStatus);
+Thread thread("status_thread", fetchStatus);
 
 int green[3] = {52, 208, 88};
 int red[3] = {215, 58, 73};
@@ -30,12 +30,16 @@ void setup() {
 }
 
 void loop() {
-  if (currentStatus == good) {
+  switch(status) {
+  case Status::good:
     everythingsOK();
-  } else if (currentStatus == minor) {
+    break;
+  case Status::minor:
     uhOh();
-  } else {
+    break;
+  case Status::major:
     rip();
+    break;
   }
 }
 
@@ -100,18 +104,19 @@ void fetchStatus() {
   while (true) {
     Serial.println("fetching...");
     Particle.publish("status/check");
-    os_thread_delay_until(&lastThreadTime, 30000);
+    os_thread_delay_until(&lastThreadTime, 3000);
   }
 }
 
 void statusHandler(const char *event, const char *data) {
   if (data) {
-    if (data == "good") {
-      currentStatus = good;
-    } else if (data == "minor") {
-      currentStatus = minor;
-    } else if (data == "major") {
-      currentStatus = major;
+    Serial.println(data);
+    if (strcmp(data, "good") == 0) {
+      status = Status::good;
+    } else if (strcmp(data, "minor") == 0) {
+      status = Status::minor;
+    } else if (strcmp(data, "major") == 0) {
+      status = Status::major;
     }
   }
 }
