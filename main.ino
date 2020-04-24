66f41b92522c34bcefca366106f81fb3cae8eae3
Mike Kavouras <mikekavouras@github.com>
2020-04-24 18:48:59 -0400

    Update

--- main.ino
+++ main.ino
@@ -1,9 +1,9 @@
 #include <neopixel.h>
 
 SYSTEM_MODE(AUTOMATIC);
-// SYSTEM_THREAD(ENABLED);
+SYSTEM_THREAD(ENABLED);
 
-#define PIXEL_PIN A5
+#define PIXEL_PIN A3
 #define PIXEL_COUNT 1
 #define PIXEL_TYPE WS2812
 
@@ -12,9 +12,9 @@ Status status = Status::good;
 
 Thread thread("status_thread", fetchStatus);
 
-int green[3] = {52, 208, 88};
-int red[3] = {215, 58, 73};
-int yellow[3] = {170, 211, 61};
+int green[3] = {70, 210, 30};
+int red[3] = {215, 10, 0};
+int yellow[3] = {200, 170, 0};
 
 system_tick_t lastThreadTime = 0;
 
@@ -47,14 +47,16 @@ void everythingsOK() {
   strip.setPixelColor(0, green[0], green[1], green[2]);
   strip.setBrightness(30);
   strip.show();
-  for (int i = 30; i <= 255; i++) {
+  for (int i = 30; i <= 160; i++) {
     strip.setPixelColor(0, green[0], green[1], green[2]);
     strip.setBrightness(i);
     strip.show();
     delay(10);
   }
+  
+  delay(800);
 
-  for (int i = 255; i >= 30; i--) {
+  for (int i = 160; i >= 30; i--) {
     strip.setPixelColor(0, green[0], green[1], green[2]);
     strip.setBrightness(i);
     strip.show();
@@ -66,14 +68,14 @@ void uhOh() {
   strip.setPixelColor(0, yellow[0], yellow[1], yellow[2]);
   strip.setBrightness(30);
   strip.show();
-  for (int i = 30; i <= 255; i++) {
+  for (int i = 30; i <= 200; i++) {
     strip.setPixelColor(0, yellow[0], yellow[1], yellow[2]);
     strip.setBrightness(i);
     strip.show();
     delay(2);
   }
 
-  for (int i = 255; i >= 30; i--) {
+  for (int i = 200; i >= 30; i--) {
     strip.setPixelColor(0, yellow[0], yellow[1], yellow[2]);
     strip.setBrightness(i);
     strip.show();
@@ -82,7 +84,7 @@ void uhOh() {
 }
 
 void rip() {
-  strip.setBrightness(255);
+  strip.setBrightness(200);
   strip.setPixelColor(0, red[0], red[1], red[2]);
   strip.show();
   delay(200);
@@ -90,7 +92,7 @@ void rip() {
   strip.setPixelColor(0, red[0], red[1], red[2]);
   strip.show();
   delay(200);
-  strip.setBrightness(255);
+  strip.setBrightness(200);
   strip.setPixelColor(0, red[0], red[1], red[2]);
   strip.show();
   delay(200);
@@ -102,16 +104,15 @@ void rip() {
 
 void fetchStatus() {
   while (true) {
-    Serial.println("fetching...");
     Particle.publish("status/check");
-    os_thread_delay_until(&lastThreadTime, 30000);
+    os_thread_delay_until(&lastThreadTime, 20000);
   }
 }
 
 void statusHandler(const char *event, const char *data) {
   if (data) {
     Serial.println(data);
-    if (strcmp(data, "good") == 0) {
+    if (strcmp(data, "none") == 0) {
       status = Status::good;
     } else if (strcmp(data, "minor") == 0) {
       status = Status::minor;
