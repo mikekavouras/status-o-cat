bca0c4940d8bf6e0505575ac68fc72802c738b3b
Mike Kavouras <mikekavouras@github.com>
2020-12-16 23:25:05 -0500

    Add maitenance mode

--- main.ino
+++ main.ino
@@ -7,14 +7,15 @@ SYSTEM_THREAD(ENABLED);
 #define PIXEL_COUNT 1
 #define PIXEL_TYPE WS2812
 
-enum Status { good, minor, major};
+enum Status { good, minor, major, maintenance};
 Status status = Status::good;
 
 Thread thread("status_thread", fetchStatus);
 
-int green[3] = {70, 210, 30};
+int green[3] = {80, 210, 10};
 int red[3] = {215, 10, 0};
 int yellow[3] = {200, 170, 0};
+int blue[3] = {19, 105, 207};
 
 system_tick_t lastThreadTime = 0;
 
@@ -34,12 +35,36 @@ void loop() {
   case Status::good:
     everythingsOK();
     break;
+  case Status::maintenance:
+    maintaining();
+    break;
   case Status::minor:
     uhOh();
     break;
   case Status::major:
     rip();
     break;
+  }
+}
+
+void maintaining() {
+  strip.setPixelColor(0, blue[0], blue[1], blue[2]);
+  strip.setBrightness(30);
+  strip.show();
+  for (int i = 30; i <= 160; i++) {
+    strip.setPixelColor(0, blue[0], blue[1], blue[2]);
+    strip.setBrightness(i);
+    strip.show();
+    delay(10);
+  }
+
+  delay(800);
+
+  for (int i = 160; i >= 30; i--) {
+    strip.setPixelColor(0, blue[0], blue[1], blue[2]);
+    strip.setBrightness(i);
+    strip.show();
+    delay(10);
   }
 }
 
@@ -53,7 +78,7 @@ void everythingsOK() {
     strip.show();
     delay(10);
   }
-  
+
   delay(800);
 
   for (int i = 160; i >= 30; i--) {
@@ -114,6 +139,8 @@ void statusHandler(const char *event, const char *data) {
     Serial.println(data);
     if (strcmp(data, "none") == 0) {
       status = Status::good;
+    } else if (strcmp(data, "maintenance") == 0) {
+      status = Status::maintenance;
     } else if (strcmp(data, "minor") == 0) {
       status = Status::minor;
     } else if (strcmp(data, "major") == 0) {
